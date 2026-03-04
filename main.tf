86d758d44a4ec2831acd7a6f3b3851aab3e5459b
Aparna Badireddigari <aparna.badireddy@gmail.com>
2026-03-04 22:34:37 +0530

    Updated source

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@
 data "azapi_client_config" "current" {}
 
 module "avm_ptn_alz_default" {
-  source             = "Azure/avm-ptn-alz/azurerm/examples/default"
+  source             = "Azure/avm-ptn-alz/azurerm"
+  version            = var.alz_module_version
   architecture_name  = "alz"
   location           = "eastus"
   parent_resource_id = data.azapi_client_config.current.tenant_id

1fa8cf235cd01c6c94cbc7abc58c6bb3b37a7b4c
Aparna Badireddigari <aparna.badireddy@gmail.com>
2026-03-04 22:36:54 +0530

    Updated values

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ data "azapi_client_config" "current" {}
 
 module "avm_ptn_alz_default" {
   source             = "Azure/avm-ptn-alz/azurerm"
-  version            = var.alz_module_version
+  version            = "0.19.0"
   architecture_name  = "alz"
   location           = "eastus"
   parent_resource_id = data.azapi_client_config.current.tenant_id
-  enable_telemetry   = var.enable_telemetry
+  enable_telemetry   = true
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable "alz_module_version" {
-  description = "Version of the Azure AVM ALZ pattern module example to deploy."
-  type        = string
-  default     = "0.19.0"
-}
+# variable "alz_module_version" {
+#   description = "Version of the Azure AVM ALZ pattern module example to deploy."
+#   type        = string
+#   default     = "0.19.0"
+# }
 
-variable "enable_telemetry" {
-  description = "Enable telemetry for the ALZ module."
-  type        = bool
-  default     = true
-}
+# variable "enable_telemetry" {
+#   description = "Enable telemetry for the ALZ module."
+#   type        = bool
+#   default     = true
+# }

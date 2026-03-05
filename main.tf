65f35190f03c6c6cd98bd455026522734432173a
Aparna Badireddigari <aparna.badireddy@gmail.com>
2026-03-05 15:52:04 +0530

    Updated code

--- main.tf
+++ main.tf
@@ -10,4 +10,23 @@ module "avm_ptn_alz_default" {
   location           = "eastus"
   parent_resource_id = data.azapi_client_config.current.tenant_id
   enable_telemetry   = true
+
+  retries = {
+    management_groups = {
+      error_message_regex = [
+        "AuthorizationFailed",
+        "context deadline exceeded",
+        "Permission to Microsoft.Management/managementGroups"
+      ]
+      interval_seconds     = 10
+      max_interval_seconds = 60
+    }
+  }
+
+  timeouts = {
+    management_group = {
+      create = "120m"
+      read   = "120m"
+    }
+  }
 }

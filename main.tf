dcc3245f0a4a804e370a5824b33670790890a8a9
Aparna Badireddigari <aparna.badireddy@gmail.com>
2026-03-05 17:17:58 +0530

    updated code

--- main.tf
+++ main.tf
@@ -35,3 +35,9 @@ module "avm-ptn-alz-connectivity-hub-and-spoke-vnet" {
   source  = "Azure/avm-ptn-alz-connectivity-hub-and-spoke-vnet/azurerm"
   version = "0.16.11"
 }
+
+
+module "avm-ptn-alz-connectivity-hub-and-spoke-vnet_example_basic-options-and-single-region" {
+  source  = "Azure/avm-ptn-alz-connectivity-hub-and-spoke-vnet/azurerm//examples/basic-options-and-single-region"
+  version = "0.16.11"
+}

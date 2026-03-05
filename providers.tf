3f5d2b28274dea5209b43112610fc616d0b011ef
Aparna Badireddigari <aparna.badireddy@gmail.com>
2026-03-05 11:17:30 +0530

    Updated providers

--- providers.tf
+++ providers.tf
@@ -13,4 +13,14 @@ provider "alz" {
 
 terraform {
   required_version = ">= 1.9, < 2.0"
+
+  required_providers {
+    azapi = {
+      source = "azure/azapi"
+    }
+
+    alz = {
+      source = "azure/alz"
+    }
+  }
 }

c7ee782e2de3ef54ad8e7a33d46a75564e7918a6
Aparna Badireddigari <aparna.badireddy@gmail.com>
2026-03-05 11:33:05 +0530

    Updated providers

--- providers.tf
+++ providers.tf
@@ -1,26 +1,35 @@
+locals {
+  alz_library_references = concat(
+    [
+      {
+        path = "platform/alz"
+        ref  = "2025.09.0"
+      }
+    ],
+    fileexists("${path.root}/lib") ? [
+      {
+        custom_url = "${path.root}/lib"
+      }
+    ] : []
+  )
+}
+
 provider "alz" {
   library_overwrite_enabled = true
-  library_references = [
-    {
-      path = "platform/alz",
-      ref  = "2025.09.0"
-    },
-    {
-      custom_url = "${path.root}/lib"
-    }
-  ]
+  library_references        = local.alz_library_references
 }
 
 terraform {
   required_version = ">= 1.9, < 2.0"
 
   required_providers {
-    azapi = {
-      source = "azure/azapi"
-    }
-
     alz = {
-      source = "azure/alz"
+      source  = "azure/alz"
+      version = "~> 0.20"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 2.0, >= 2.0.1"
     }
   }
 }

203710ef41a9c2a7603ec093aa7d1e209a39b224
Aparna Badireddigari <aparna.badireddy@gmail.com>
2026-03-11 19:15:48 +0530

    updated ARC runners

---
+++ alz/main.tf
@@ -0,0 +1,19 @@
+
+data "azapi_client_config" "current" {}
+
+module "avm-ptn-alz-connectivity-hub-and-spoke-vnet" {
+  source  = "Azure/avm-ptn-alz-connectivity-hub-and-spoke-vnet/azurerm"
+  version = "0.16.11"
+}
+
+
+module "avm-ptn-alz-connectivity-hub-and-spoke-vnet_example_basic-options-and-single-region" {
+  source  = "Azure/avm-ptn-alz-connectivity-hub-and-spoke-vnet/azurerm//examples/basic-options-and-single-region"
+  version = "0.16.11"
+}
+
+
+module "avm-ptn-alz-connectivity-hub-and-spoke-vnet_example_full-multi-region" {
+  source  = "Azure/avm-ptn-alz-connectivity-hub-and-spoke-vnet/azurerm//examples/full-multi-region"
+  version = "0.16.11"
+}
---
+++ alz/providers.tf
@@ -0,0 +1,47 @@
+locals {
+  alz_library_references = concat(
+    [
+      {
+        path = "platform/alz"
+        ref  = "2025.09.0"
+      }
+    ],
+    fileexists("${path.root}/lib") ? [
+      {
+        custom_url = "${path.root}/lib"
+      }
+    ] : []
+  )
+}
+
+terraform {
+  required_version = ">= 1.9, < 2.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0.0, < 5.0.0"
+    }
+    alz = {
+      source  = "azure/alz"
+      version = "~> 0.20"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 2.0, >= 2.0.1"
+    }
+  }
+}
+
+provider "alz" {
+  library_overwrite_enabled = true
+  library_references        = local.alz_library_references
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}

def36cec01370aace0488490b669c4f3b73b2ff9
Aparna Badireddigari <aparna.badireddy@gmail.com>
2026-03-25 21:30:16 +0530

    updated alz code

--- alz/main.tf
+++ alz/main.tf
@@ -7,10 +7,10 @@ module "avm-ptn-alz-connectivity-hub-and-spoke-vnet" {
 }
 
 
-# # module "avm-ptn-alz-connectivity-hub-and-spoke-vnet_example_basic-options-and-single-region" {
-# #   source  = "Azure/avm-ptn-alz-connectivity-hub-and-spoke-vnet/azurerm//examples/basic-options-and-single-region"
-# #   version = "0.16.11"
-# # }
+module "avm-ptn-alz-connectivity-hub-and-spoke-vnet_example_basic-options-and-single-region" {
+  source  = "Azure/avm-ptn-alz-connectivity-hub-and-spoke-vnet/azurerm//examples/basic-options-and-single-region"
+  version = "0.16.11"
+}
 
 
 # # module "avm-ptn-alz-connectivity-hub-and-spoke-vnet_example_full-multi-region" {

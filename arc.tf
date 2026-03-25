f05fd00d93e5a4d474001ef78505efa91a58b9ef
Aparna Badireddigari <aparna.badireddy@gmail.com>
2026-03-25 15:35:09 +0530

    updated code

--- arc.tf
+++ arc.tf
@@ -6,7 +6,7 @@
 
 # Read AKS created in main.tf (module.default)
 data "azurerm_kubernetes_cluster" "aks" {
-  name                = local.aks_name_default
+  name                = local.aks_name_automatic
   resource_group_name = azurerm_resource_group.this.name
   depends_on          = [module.automatic]
 }
